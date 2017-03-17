b93eec4151a3d62cf9baf3b0e2f794864b9d214a
francesco gobbi <francesco gobbi>
2017-03-17 07:31:40 +0100

    Creazione classe per i barcode

---
+++ barcode.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: francesco
+ * Date: 16/03/2017
+ * Time: 14:57
+ */
+
+class barcode {
+    private $height;
+    private $width;
+    private $barheight;
+    private $barwidth;
+    private $label;
+    private $border;
+    private $padding;
+    private $mapcode;
+    private $rightmap;
+    private $code;
+
+    function __construct($label = null,$barwidth = 2){
+        $this->set_width($barwidth);
+        $this->set_label($label);
+        $this->padding = $barwidth*10;
+        $this->border = 2;
+        $this->mapcode = array(
+            '0'=>'0001101', '1'=>'0011001','2'=>'0010011','3'=>'0111101',
+            '4'=>'0100011', '5'=>'0110001','6'=>'0101111','7'=>'0111011',
+            '8'=>'0110111', '9'=>'0001011','#'=>'01010','*'=>'101'
+        );
+        $this->rightmap = array(
+            '0'=>'1110010', '1'=>'1100110','2'=>'1101100','3'=>'1000010',
+            '4'=>'1011100', '5'=>'1001110','6'=>'1010000','7'=>'1000100',
+            '8'=>'1001000', '9'=>'1110100','#'=>'01010','*'=>'101'
+        );
+    }
+
+    public function set_width($barwidth){
+        if(is_int($barwidth)){
+            $this->barwidth = $barwidth;
+        } else {
+            $this->barwidth=2;
+        }
+    }
+
+    public function set_label($label){
+        if(is_null($label)){
+            $this->label = "Open Barcode";
+        } else {
+            $this->label = $label;
+        }
+    }
+
+    public function build($code = null, $name=null){
+        if(is_null($code)){
+            $this->code = '00000000000';
+        } else {
+            $this->code = $code;
+        }
+
+        $this->code = substr($code, 0,11);
+
+        if(is_numeric($code)){
+            $checksum = 0;
+
+            for($digit =0; $digit<strlen($code);$digit++){
+                if(($digit%2) == 0){
+                    $checksum += (int)$code[$digit]*3;
+                } else {
+                    $checksum += (int) $code[$digit];
+                }
+            }
+
+            $checkdigit = 10 - $checksum%10;
+
+            $this->code.=$checkdigit;
+            $code_display = $code;
+            $code = "*".substr($code,0,6)."#".substr($code,6,6)."*";
+
+            $this->width = $this->barwidth*95+$this->padding*2;
+            $this->barheight =  $this->barwidth*95*0.75;
+            $this->height = $this->barheight+$this->padding*2;
+
+            $barcode = imagecreatetruecolor($this->width,$this->barheight);
+
+            $black = imagecolorallocate($barcode,0,0,0);
+            $white = imagecolorallocate($barcode,255,255,255);
+            imagefill($barcode,0,0,$black);
+
+            imagefilledrectangle($barcode,$this->border,$this->border,$this->width - $this->border - 1,$this->barheight - $this->border -1, $white);
+
+            $bar_position = $this->padding;
+            for ($count = 0;$count<15; $count++){
+                $character = $code [$count];
+                for ($subcount = 0;$subcount <strlen($this->mapcode[$character]);$subcount++){
+                    if($count < 7){
+                        $color = ($this->mapcode[$character][$subcount]==0) ? $white : $black;
+                    } else {
+                        $color = ($this->rightmap[$character][$subcount]==0) ? $white : $black;
+                    }
+                    
+                    if(strlen($this->mapcode[$character])==7){
+                        $height_offset = $this->height * 0.05;
+                    } else {
+                        $height_offset = 0;
+                    }
+                    
+                    imagefilledrectangle($barcode,$bar_position,$this->padding,$bar_position+$this->barwidth-1,$this->barheight-$height_offset-$this->padding,$color);
+                    $bar_position +=$this->barwidth;
+                }
+            }
+
+            /*$font = "Aller/Aller_Rg.ttf";
+
+            $digit1 = substr($code_display,0,1);
+            $leftdigits = substr($code_display,1,5);
+            $rightdigits = substr($code_display,6,5);
+            $digit12 = substr($code_display,11,1);
+
+            $outterdigitsscale = $this->barwidth*5;
+            $innerdigitsscale = $this->barwidth/0.14;
+            $labelscale = $this->barwidth/0.22;
+
+            $digit1_xy = imagettfbbox($outterdigitsscale,0,$font,$digit1);
+            $left_digits_xy = imagettfbbox($innerdigitsscale,0,$font,$leftdigits);
+            $right_digits_xy = imagettfbbox($innerdigitsscale,0,$font,$rightdigits);
+            $digit12_xy = imagettfbbox($innerdigitsscale,0,$font,$digit12);
+            $label_xy = imagettfbbox($labelscale,0,$font,$this->label);
+
+            $middleSpace = 43*$this->barwidth;
+
+            $digit1_x =$this->border+($this->padding);
+
+            + parte per i numeri
+
+            */
+
+
+
+            header("Content-type: image/gif");
+            imagepng($barcode);
+            imagedestroy($barcode);
+        }
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: francesco
+ * Date: 16/03/2017
+ * Time: 19:22
+ */
+include "barcode.php";
+$bar = new barcode(null,4);
+$number = "36627634746";
+$bar->build($number);
